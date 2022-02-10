41a036abf24ba5be17a2af6144e448c11ed11ced
Karla Salinas <salinasdiazkarla@gmail.com>
2022-02-10 17:30:02 -0600

    10 feb

---
+++ HerenciaYPolimorfismo/src/com/generation/animales/Main.java
@@ -0,0 +1,16 @@
+package com.generation.animales;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Perro lomito = new Perro();
+		lomito.comer();
+		
+		Vaca vaca1 = new Vaca();
+		vaca1.dormir();
+		
+		Gato michi = new Gato();
+		michi.dormir();
+	}
+
+}
